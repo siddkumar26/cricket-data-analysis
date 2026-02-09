3c72f5c3d3da3943ba2ec7b4c781e200639eda44
siddkumar26 <67642844+siddkumar26@users.noreply.github.com>
2026-02-09 14:04:33 +0000

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,118 @@
+USE CricketWarehouse;
+GO
+
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'silver' Tables
+===============================================================================
+*/
+
+
+-- Create Ball By Ball data table
+IF OBJECT_ID('silver.deliveries_updated_ipl_upto_2025', 'U') IS NOT NULL
+    DROP TABLE silver.deliveries_updated_ipl_upto_2025;
+GO
+
+CREATE TABLE silver.deliveries_updated_ipl_upto_2025 (
+    ball_id INT NOT NULL,
+    match_id INT,
+    [match_date] DATE,
+    inning INT,
+    over_ball FLOAT,
+    [over_number] INT,
+    ball_in_over INT,
+    batting_team NVARCHAR(50),
+    bowling_team NVARCHAR(50),
+    batter NVARCHAR(50),
+    non_striker NVARCHAR(50),
+    bowler NVARCHAR(50),
+    batter_runs INT,
+    extras INT,
+    wide_runs INT,
+    no_ball_runs INT,
+    byes_runs INT,
+    leg_byes_runs INT,
+    penalty_runs INT,
+    dismissal_kind NVARCHAR(50),
+    player_dismissed NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE(),
+    CONSTRAINT PK_Silver_Deliveries PRIMARY KEY CLUSTERED (ball_id)
+);
+
+-- Create basic info data table
+IF OBJECT_ID('silver.matches_updated_ipl_upto_2025', 'U') IS NOT NULL
+    DROP TABLE silver.matches_updated_ipl_upto_2025;
+GO
+
+-- Removing date1 and date2 as they hold no useful information
+-- Removing Event as it is all IPL
+-- Removing balls per over as its always 6
+CREATE TABLE silver.matches_updated_ipl_upto_2025 (
+    -- Primary Key
+    match_id INT NOT NULL,
+
+    -- Season Number
+    season NVARCHAR(20),
+
+    match_number INT,
+    [match_date] DATE,
+    venue NVARCHAR(100),
+    city NVARCHAR(50),
+
+    first_team NVARCHAR(50),
+    second_team NVARCHAR(50),
+
+    outcome NVARCHAR(50),
+    toss_winner NVARCHAR(50),
+    toss_decision NVARCHAR(50),
+    match_winner NVARCHAR(50),
+    player_of_match NVARCHAR(50),
+
+    winner_runs INT,
+    winner_wickets INT,
+
+    umpire_1 NVARCHAR(50),
+    umpire_2 NVARCHAR(50),
+    reserve_umpire NVARCHAR(50),
+    tv_umpire NVARCHAR(50),
+    match_referee NVARCHAR(50),
+
+    dls_method NVARCHAR(20),
+    neutral_venue NVARCHAR(10),
+    dwh_create_date DATETIME2 DEFAULT GETDATE(),
+
+
+    CONSTRAINT PK_Silver_Matches PRIMARY KEY CLUSTERED (match_id),
+    CONSTRAINT CK_Neutral_Venue_Values CHECK (neutral_venue IN ('Yes', 'No')),
+    CONSTRAINT CK_DLS_Method_Values CHECK (dls_method IN ('D/L', 'No D/L')),
+    CONSTRAINT CK_Season_Format CHECK (season LIKE '[0-9][0-9][0-9][0-9]%' )
+);
+
+-- Create basic info data table
+IF OBJECT_ID('silver.batting_stats', 'U') IS NOT NULL
+    DROP TABLE silver.batting_stats;
+GO
+-- Creating new table for batting statistics
+CREATE TABLE silver.batting_stats (
+    player_id INT,
+    player_name NVARCHAR(50),
+    matches_played INT,
+    not_outs INT,
+    runs_scored INT,
+    highest_score INT,
+    batting_average INT,
+    batting_strike_rate INT,
+    balls_faced INT,
+    centuries INT,
+    fifties INT,
+    thirties INT,
+    twenties INT,
+    fours INT,
+    sixes INT,
+    catches INT
+)
